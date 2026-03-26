4cdfcd1b023521187e90be7ff0d64e447323fd3b
Akash-Shilimkar <akashshilimkar904@gmail.com>
2026-03-26 11:05:31 +0530

    Change on Mobile View add social inks

--- Script.js
+++ Script.js
@@ -5,3 +5,16 @@ backSpeed:100,
 backDelay:1000,
 loop:true
 })
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+document.querySelectorAll('.navbar a').forEach(link => {
+    link.onclick = () => {
+        navbar.classList.remove('active');
+    };
+});
